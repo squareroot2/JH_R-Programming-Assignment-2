ba88c7eaf58116da5b3996d8560a33cc13dc7400
complex <12c311f7@opayq.com>
2014-09-22 11:20:16 +1200

    Added comments, Fitted sample unit test code of TA Horne

--- testingCacheMatrix.R
+++ testingCacheMatrix.R
@@ -1,6 +1,10 @@
+# Testing my cachematrix code
+
 source("cachematrix.R")
-# matrix(1:2:9, nrow = 3, ncol = 3, byrow=TRUE) 
-rm(aMatrix)
+
+rm(aMatrix) # initialise
+
+# create and view matrix and its properties
 aMatrix <- c(6, 2, 4, 8, 4, 10, 2, 16, 14) 
 aMatrix
 dim(aMatrix) <- c(3,3)
@@ -8,11 +12,61 @@ aMatrix
 class(aMatrix)
 solve(aMatrix)
 
-rm(cm)
+# now create makeCacheMatrix object from given meatrix
+rm(cm) # initialise
 cm <- makeCacheMatrix(aMatrix)
 cm
-cm$get()
+cm$get() # view the matrix
 class(cm)
 str(cm)
 
+# create the matrix inverse of the given matrix
 cacheSolve(cm)
+
+
+####################
+
+# Gregory D. HorneCommunity TA· R Programming Coursera course. Sep 2014
+# Unit tests (with expected output) for Programming Assignment 2
+
+### Example
+
+      source("cachematrix.R")
+
+     amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+    amatrix$get()         # Returns original matrix
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+
+    cacheSolve(amatrix)   # Computes, caches, and returns    matrix inverse
+    [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+
+   amatrix$getInverse()  # Returns matrix inverse
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+
+   cacheSolve(amatrix)   # Returns cached matrix inverse using previously computed matrix inverse
+getting cached data
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+
+    amatrix$set(matrix(c(0,5,99,66), nrow=2, ncol=2)) # Modify existing matrix
+    cacheSolve(amatrix)   # Computes, caches, and returns new matrix inverse
+            [,1] [,2]
+[1,] -0.13333333  0.2
+[2,]  0.01010101  0.0
+
+    amatrix$get()         # Returns matrix
+     [,1] [,2]
+[1,]    0   99
+[2,]    5   66
+
+    amatrix$getInverse()  # Returns matrix inverse
+            [,1] [,2]
+[1,] -0.13333333  0.2
+[2,]  0.01010101  0.0
